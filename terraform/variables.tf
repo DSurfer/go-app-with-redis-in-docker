9607982a52903679e499954774d24ff3f9e50701
DSurfer <lolo80996@gmail.com>
2023-09-15 13:42:50 +0300

    creating instance for future deploy using terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,37 @@
+//declaring variables for auth and instance settings
+
+variable "user_name" {
+  type = string
+  sensitive = false
+}
+
+variable "password" {
+  type = string
+  sensitive = true
+}
+
+variable "api_key" {
+  type = string
+  description = "Token for your project"
+  sensitive = true
+}
+
+variable "image_flavor" {
+    type = string
+    default = "Ubuntu-18.04-Standard"
+}
+
+variable "compute_flavor" {
+  type = string
+  default = "Basic-1-1-10"
+}
+
+variable "key_pair_name" {
+  type = string
+  default = "testing"
+}
+
+variable "availability_zone_name" {
+  type = string
+  default = "MS1"
+}
